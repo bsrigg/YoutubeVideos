647731483f10b618e68ec1a860ae47ec171e77a7
Barry S Rigg <barrysrigg@gmail.com>
2019-06-28 15:55:33 -0400

    renamed build to docs for github pages deployment

---
+++ docs/precache-manifest.fba68c33eaeeb62a8660640355733478.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b55f555309f3264e04aec21d4a49f5f",
+    "url": "/YoutubeVideos/index.html"
+  },
+  {
+    "revision": "f8530eb3842d2b3d63ac",
+    "url": "/YoutubeVideos/static/css/main.b8dd62d4.chunk.css"
+  },
+  {
+    "revision": "7e6ac1444ef9242ebc00",
+    "url": "/YoutubeVideos/static/js/2.824d5390.chunk.js"
+  },
+  {
+    "revision": "f8530eb3842d2b3d63ac",
+    "url": "/YoutubeVideos/static/js/main.7e359a5e.chunk.js"
+  },
+  {
+    "revision": "90604308a09ec34a4c76",
+    "url": "/YoutubeVideos/static/js/runtime~main.fc2d257b.js"
+  }
+]);
